a4c070565b50af7073f2f88b4147af69bdc5b1f9
dkwasnik <dawid.kwach@gmail.com>
2024-08-17 11:45:26 +0200

    separate main contract to new file

---
+++ example/lib/AppCentral.dart
@@ -0,0 +1,33 @@
+
+import 'package:universal_chess_driver/Central.dart';
+import 'package:flutter_chess_board/flutter_chess_board.dart';
+
+class AppCentral implements Central {
+  ChessBoardController _chessController;
+  String lastPeripheralMove = "";
+
+  AppCentral(this._chessController);
+
+  Future<bool> move(String uci) {
+    if (!isMoveLegal(uci))
+      return Future.value(false);
+
+    lastPeripheralMove = uci;
+    _chessController.makeMoveUci(uci: uci);
+    return Future.value(true);
+  }
+  Future<String> obtainPromotedPawn() { return Future.value("q"); }
+  void indicateOutOfSync(String peripherialFen) { print("Peripherial is out of sync: $peripherialFen"); }
+  void showMsg(String msg) { print("Peripherial is out of sync: $msg"); }
+
+  bool isMoveLegal(String move) {
+    String src = move.substring(0, 2);
+    String dst = move.substring(2, 4);
+    String promotion = move.substring(4);
+    return _chessController.game.moves({"asObjects": true}).any((m) =>
+        src == m.fromAlgebraic &&
+        dst == m.toAlgebraic &&
+        promotion == (m.promotion?.name ?? ""));
+  }
+
+}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,46 +1,15 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_chess_board/flutter_chess_board.dart';
 import 'package:example/ble/BleClientProvider.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:universal_chess_driver/Peripherial.dart';
-import 'package:universal_chess_driver/Central.dart';
+import 'package:example/AppCentral.dart';
 import 'package:universal_chess_driver/CecpPeripherial.dart';
 import 'package:universal_chess_driver/BleClient.dart';
 import 'package:example/ble/Scanner.dart';
 
 void main() {
   runApp(MyApp());
-}
-
-class AppCentral implements Central {
-  ChessBoardController _chessController;
-  String lastPeripheralMove = "";
-
-  AppCentral(this._chessController);
-
-  Future<bool> move(String uci) {
-    if (!isMoveLegal(uci))
-      return Future.value(false);
-
-    lastPeripheralMove = uci;
-    _chessController.makeMoveUci(uci: uci);
-    return Future.value(true);
-  }
-  Future<String> obtainPromotedPawn() { return Future.value("q"); }
-  void indicateOutOfSync(String peripherialFen) { print("Peripherial is out of sync: $peripherialFen"); }
-  void showMsg(String msg) { print("Peripherial is out of sync: $msg"); }
-
-  bool isMoveLegal(String move) {
-    String src = move.substring(0, 2);
-    String dst = move.substring(2, 4);
-    String promotion = move.substring(4);
-    return _chessController.game.moves({"asObjects": true}).any((m) =>
-        src == m.fromAlgebraic &&
-        dst == m.toAlgebraic &&
-        promotion == (m.promotion?.name ?? ""));
-  }
 }
 
 class MyApp extends StatelessWidget {
