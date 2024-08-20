d47c283c1292f9efa86387aae14918630738f50c
dkwasnik <dawid.kwach@gmail.com>
2024-08-20 21:32:17 +0200

    use ble utils from device provider

---
+++ example/lib/game_screen.dart
@@ -0,0 +1,98 @@
+import 'package:ble_device_provider/ble_connector.dart';
+import 'package:example/app_central.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_chess_board/flutter_chess_board.dart';
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:universal_chess_driver/ble_client.dart';
+import 'package:universal_chess_driver/cecp_peripherial.dart';
+import 'package:universal_chess_driver/peripherial.dart';
+
+class GameScreen extends StatefulWidget {
+  GameScreen(FlutterReactiveBle this.ble, {required this.device, super.key})
+      : bleConnector = BleConnector(ble, deviceId: device.id);
+
+  final FlutterReactiveBle ble;
+  final DiscoveredDevice device;
+  final BleConnector bleConnector;
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  ChessBoardController chessController = ChessBoardController();
+  late AppCentral appCentral;
+  late Peripherial? peripherialBoard;
+
+  BleConnector get bleConnector => widget.bleConnector;
+  FlutterReactiveBle get ble => widget.ble;
+  DiscoveredDevice get device => widget.device;
+
+  void onRequestNewGame() {
+    chessController.resetBoard();
+    peripherialBoard?.startNewGame(chessController.getFen(), "standard");
+  }
+
+  Widget connectedBoardButtons() {
+    return Column(
+      children: [
+        SizedBox(height: 25),
+        TextButton(onPressed: onRequestNewGame, child: Text("Request New game")),
+      ],
+    );
+  }
+
+  void _onConnectionStateChanged(BleConnectionState state) {
+    setState(() {
+      if (state == BleConnectionState.disconnected) {
+        bleConnector.findAndConnect(Uuid.parse(Bleclient.srv));
+        peripherialBoard = null;
+      } else if (state == BleConnectionState.connected) {
+        setState(() {
+          var client = Bleclient(ble, device);
+          peripherialBoard = CecpPeripherial(client, appCentral);
+        });
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    appCentral = AppCentral(chessController);
+    bleConnector.stateStream.listen(_onConnectionStateChanged);
+    chessController.addListener(() {
+      if (chessController.game.history.isEmpty) return;
+
+      Move lastMove = chessController.game.history.last.move;
+      String lastMoveUci = lastMove.fromAlgebraic + lastMove.toAlgebraic;
+      if (lastMove.promotion != null) lastMoveUci = lastMoveUci + lastMove.promotion!.name;
+
+      if (lastMoveUci != appCentral.lastPeripheralMove) peripherialBoard?.move(lastMoveUci);
+    });
+    bleConnector.connect();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Column(
+      children: [
+        connectedBoardButtons(),
+        ChessBoard(
+          controller: chessController,
+          boardColor: BoardColor.darkBrown,
+          boardOrientation: PlayerColor.white,
+        )
+      ],
+    );
+
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+            appBar: PreferredSize(
+              preferredSize: const Size.fromHeight(100),
+              child: AppBar(title: Text("Universal chess board example")),
+            ),
+            body: content));
+  }
+}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,144 +1,44 @@
-import 'package:example/app_central.dart';
-import 'package:example/ble/ble_client_provider.dart';
-import 'package:example/ble/scanner.dart';
+import 'package:ble_device_provider/scanner_screen.dart';
+import 'package:example/game_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_chess_board/flutter_chess_board.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:universal_chess_driver/ble_client.dart';
-import 'package:universal_chess_driver/cecp_peripherial.dart';
-import 'package:universal_chess_driver/peripherial.dart';
 
+FlutterReactiveBle ble = FlutterReactiveBle();
 void main() {
-  runApp(MyApp());
+  runApp(UniversalDriverDemoApp());
 }
 
-class MyApp extends StatelessWidget {
+class UniversalDriverDemoApp extends StatelessWidget {
+  UniversalDriverDemoApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Universal chess driver example',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.indigo,
+          brightness: Brightness.light,
+        ),
       ),
-      home: MyHomePage(),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.indigo,
+          brightness: Brightness.dark,
+        ),
+      ),
+      home: ScannerScreen(
+          ble,
+          Uuid.parse(Bleclient.srv),
+          (device, context) async => await Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => GameScreen(ble, device: device)),
+              ),
+          null),
+      debugShowCheckedModeBanner: false,
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  BleClientProvider bleProvider = BleClientProvider();
-  Peripherial? peripherialBoard;
-  ChessBoardController chessController = ChessBoardController();
-  late AppCentral appCentral;
-  late List<DiscoveredDevice> _chessBoardsDevices;
-
-  _MyHomePageState() {
-    appCentral = AppCentral(chessController);
-    bleProvider.connectionState.listen((event) {
-      if (event.connectionState == DeviceConnectionState.connected) {
-        var chessBoardDevice = _chessBoardsDevices.firstWhere((d) => d.id.toString() == event.deviceId);
-        setState(() {
-          peripherialBoard = new CecpPeripherial(bleProvider.createClient(chessBoardDevice), appCentral);
-        });
-      } else {
-        setState(() {
-          peripherialBoard = null;
-        });
-      }
-    });
-
-    chessController.addListener(() {
-      if (chessController.game.history.isEmpty) return;
-
-      Move lastMove = chessController.game.history.last.move;
-      String lastMoveUci = lastMove.fromAlgebraic + lastMove.toAlgebraic;
-      if (lastMove.promotion != null) lastMoveUci = lastMoveUci + lastMove.promotion!.name;
-
-      if (lastMoveUci != appCentral.lastPeripheralMove) peripherialBoard!.move(lastMoveUci);
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  void onRequestNewGame() {
-    chessController.resetBoard();
-    peripherialBoard!.startNewGame(chessController.getFen(), "standard");
-  }
-
-  Widget connectedBoardButtons() {
-    return Column(
-      children: [
-        SizedBox(height: 25),
-        TextButton(onPressed: onRequestNewGame, child: Text("Request New game")),
-      ],
-    );
-  }
-
-  Widget deviceList(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        SizedBox(height: 25),
-        Center(
-            child: StreamBuilder(
-                stream: bleProvider.scannerState,
-                builder: (context, AsyncSnapshot<BleScannerState> snapshot) {
-                  return (snapshot.hasData && snapshot.data!.scanIsInProgress)
-                      ? CircularProgressIndicator()
-                      : TextButton(child: Text("List Devices"), onPressed: () => bleProvider.scan(Bleclient.srv));
-                })),
-        Flexible(
-            child: StreamBuilder(
-                stream: bleProvider.scannerState,
-                builder: (context, AsyncSnapshot<BleScannerState> snapshot) {
-                  _chessBoardsDevices = snapshot.hasData ? snapshot.data!.discoveredDevices : [];
-
-                  return ListView.builder(
-                      itemCount: _chessBoardsDevices.length,
-                      itemBuilder: (context, index) => ListTile(
-                            title: Text(_chessBoardsDevices[index].name),
-                            subtitle: Text(_chessBoardsDevices[index].id.toString()),
-                            onTap: () => bleProvider.connect(_chessBoardsDevices[index].id.toString()),
-                          ));
-                })),
-        SizedBox(height: 24)
-      ],
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    Widget content = peripherialBoard == null
-        ? deviceList(context)
-        : Column(
-            children: [
-              connectedBoardButtons(),
-              ChessBoard(
-                controller: chessController,
-                boardColor: BoardColor.darkBrown,
-                boardOrientation: PlayerColor.white,
-              )
-            ],
-          );
-
-    return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-            appBar: PreferredSize(
-              preferredSize: const Size.fromHeight(100),
-              child: AppBar(title: Text("Universal chess board example")),
-            ),
-            body: content));
   }
 }
