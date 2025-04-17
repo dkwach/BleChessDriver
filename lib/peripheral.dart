9aa2e8ea679e62e5ac285acc96d5a914b3e0c033
vovagorodok <vovagorodok2@gmail.com>
2025-04-18 00:40:39 +0200

    Added commnts

--- lib/peripheral.dart
+++ lib/peripheral.dart
@@ -3,7 +3,9 @@ import 'package:universal_chess_driver/option.dart';
 abstract class Round {
   bool get isVariantSupported;
   bool get isStateSynchronized;
+  // set_state feature
   bool get isStateSetible;
+
   String? get fen;
   String? get rejectedMove;
 }
@@ -14,6 +16,7 @@ abstract class Peripheral {
 
   bool get isInitialized;
   Round get round;
+  // option feature
   bool get areOptionsInitialized;
   List<Option> get options;
 
@@ -23,12 +26,18 @@ abstract class Peripheral {
   Stream<bool> get stateSynchronizeStream;
   Stream<String> get moveStream;
   Stream<String> get errStream;
+  // msg feature
   Stream<String> get msgStream;
+  // undo feature
   Stream<String> get undoStream;
+  // moved feature
   Stream<void> get movedStream;
+  // resign feature
   Stream<void> get resignStream;
+  // draw_offer feature
   Stream<void> get drawOfferStream;
   Stream<bool> get drawOfferAckStream;
+  // option feature
   Stream<void> get optionsUpdateStream;
 
   Future<void> handleBegin({
@@ -54,21 +63,28 @@ abstract class Peripheral {
   Future<void> handleErr({
     required String err,
   });
+  // msg feature
   Future<void> handleMsg({
     required String msg,
   });
+  // undo feature
   Future<void> handleUndo({
     required String move,
     String? lastMove,
     String? check,
     String? time,
   });
+  // draw_offer feature
   Future<void> handleDrawOffer();
+  // get_state feature
   Future<void> handleGetState();
+  // set_state feature
   Future<void> handleSetState();
+  // state_stream feature
   Future<void> handleState({
     required String fen,
   });
+  // option feature
   Future<void> handleOptionsBegin();
   Future<void> handleOptionsReset();
   Future<void> handleSetOption({
